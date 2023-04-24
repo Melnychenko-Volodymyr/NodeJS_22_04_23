b83232eca1c32ad5c699b844f09c9b26bc873e01
Melnychenko-Volodymyr <mvi_1968@i.ua>
2023-04-24 10:00:53 +0300

    add files

---
+++ app.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const fsp = fs.promises;
+
+console.log('START !!!');
+
+const func = async () => {
+    let text = await fsp.readFile(`${__dirname}/t1.txt`, 'utf-8');
+    text =  text + "/n" + text;
+    console.log('Read file');
+
+    await fsp.writeFile(`${__dirname}/t2.txt`, text);
+    console.log('Write file');
+
+    await fsp.copyFile(`${__dirname}/t2.txt`, `${__dirname}/t3.txt` );
+    console.log('Copy file');
+};
+
+func();
+
+console.log('FINISH !!!');
